589db128185e42c669ff603efc8a71ef220eddaa
colingalvin <colingalvin@ymail.com>
2020-09-23 11:17:38 -0500

    Create all classes, skeletons, and assign inheritance as per UML.

---
+++ MySodaMachine/Simulation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySodaMachine
+{
+    class Simulation
+    {
+        // member variables (HAS A)
+
+        // constructor (SPAWN)
+
+        // member methods (CAN DO)
+    }
+}
